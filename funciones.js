9302154cd128e1dd60b61dd0bf342e5ab245c008
jupamave <juampa270982@gmail.com>
2025-02-26 14:41:59 -0600

    video9 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1s3Z2pSSctjgKYUQbeE6c7K1BShkY1gLZv82z2YMK-_k').getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
+}
