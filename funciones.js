27dc32ee4190cb728b4111dfe360a4425f6b292e
jupamave <juampa270982@gmail.com>
2025-03-03 15:51:31 -0600

    video15 cuso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -1,16 +1,27 @@
+const HOJA = SpreadsheetApp.openById('1s3Z2pSSctjgKYUQbeE6c7K1BShkY1gLZv82z2YMK-_k').getActiveSheet();
+
 function doGet()
+{
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google App Script');
+}
+
+//funcion para usar el metodo post la cual envia los datos del formulario
+function doPost(datos)
 {
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google App Script');
 }
 
 function obtenerDatosHTML(nombre)
 {
-    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+    return HtmlService.createHtmlOutputFromFile(nombre).getContent();   
 }
 
 function obtenerContactos()
 {
-    let hoja = SpreadsheetApp.openById('1s3Z2pSSctjgKYUQbeE6c7K1BShkY1gLZv82z2YMK-_k').getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    return datos;
+    return HOJA.getDataRange().getValues();
+}
+
+function insertarContacto(nombre,correo)
+{
+    HOJA.appendRow([nombre, correo]);
 }
