466d7931e692c00df6a8484fa50aa8c24da2f8fa
jupamave <juampa270982@gmail.com>
2025-03-06 10:13:14 -0600

    video18 curso-gas-avanzado

--- funciones.js
+++ funciones.js
@@ -25,3 +25,8 @@ function insertarContacto(nombre,apellidos,correo, telf)
 {
     HOJA.appendRow([nombre, apellidos, correo, telf]);
 }
+
+function borrarContacto(numFila)
+{
+    HOJA.deleteRow(numFila);
+}
